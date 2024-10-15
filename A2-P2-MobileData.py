17b867739b8bee8468b173d09f75b5e315ddf80e
Dan Shaw <w0190983@nscc.ca>
2024-10-15 12:35:29 -0300

    Program functional. May be finished.

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -1,23 +1,41 @@
-#Program 2 – Erehwon Mobile Data Plans
-#Erewhon Mobile charges cellular customers a rate based on the total amount of data used by a customer 
-# in the billing period. For simplicity, customers are charge based on which range their total data usage 
-# falls within.
-# Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
-# then calculate the cost based on that range’s cost. 
+"""
+    Author: Dan Shaw
+    Title: Mobile Data Plans
+    Desc: Calculates the cost of a mobile data plan based on the amount of data used
+"""
 
-#Student #:     
-#Student Name:  
+from DanMath import Validation
+
+TIER_THRESHOLDS = [200, 500, 1000]
+TIER_TYPE = [0, 1, 1, 0] # 0 -> Flat Rate, 1 -> Tiered Rate
+TIER_RATES = [20, 0.105, 0.110, 118]
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    greeting = "| MOBILE DATA PLAN CALCULATOR |"
+    print("-" * len(greeting))
+    print(greeting)
+    print("-" * len(greeting))
 
+    while (dataUsed := Validation.validateInt(input("Enter the amount of data used (MB).\n> "))) == None:
+        print("You have entered an invalid input. Please enter a whole number.")
+    
+    print("Total charge is ${0:.2f}".format(getCost(dataUsed, getTier(dataUsed))))
 
+def getCost(dataUsed, tier):
+    if TIER_TYPE[tier] == 0:
+        return TIER_RATES[tier]
+    else:
+        return dataUsed * TIER_RATES[tier]
 
+def getTier(dataUsed):
+    if dataUsed <= TIER_THRESHOLDS[0]:
+        return 0
+    elif dataUsed <= TIER_THRESHOLDS[1]:
+        return 1
+    elif dataUsed <= TIER_THRESHOLDS[2]:
+        return 2
+    else:
+        return 3
 
-
-
-
-
-    # YOUR CODE ENDS HERE
-
-main()
+if __name__ == "__main__":
+    main()
