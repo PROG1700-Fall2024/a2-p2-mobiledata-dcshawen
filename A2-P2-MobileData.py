3e23b8601255dd2a377dbb80e48e9976e112c808
Dan Shaw <w0190983@nscc.ca>
2024-10-22 11:12:37 -0300

    Ready for marking

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -6,20 +6,24 @@
 
 from DanMath import Validation
 
+# Sets constants
 TIER_THRESHOLDS = [200, 500, 1000]
 TIER_TYPE = [0, 1, 1, 0] # 0 -> Flat Rate, 1 -> Tiered Rate
 TIER_RATES = [20, 0.105, 0.110, 118]
 
 def main():
+    # Outputs greeting to user
     greeting = "| MOBILE DATA PLAN CALCULATOR |"
     print("-" * len(greeting))
     print(greeting)
     print("-" * len(greeting))
 
+    # Get amount of data used from user and validate that it's a proper float value
     while (dataUsed := Validation.validateInt(input("Enter the amount of data used (MB).\n> "))) == None:
         print("You have entered an invalid input. Please enter a whole number.")
     
-    print("Total charge is ${0:.2f}".format(getCost(dataUsed, getTier(dataUsed))))
+    totalCost = getCost(dataUsed, getTier(dataUsed))
+    print("Total charge is ${0:.2f}".format(totalCost))
 
 def getCost(dataUsed, tier):
     if TIER_TYPE[tier] == 0:
